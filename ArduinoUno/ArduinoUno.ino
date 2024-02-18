30e30f50794e0a8fa142a33a08b12a253d23e660
Andrei Bertescu <andrei.bertescu2@gmail.com>
2024-02-18 17:08:34 +0200

    Changed steering code on ArduinoUno.ino

--- ArduinoUno/ArduinoUno.ino
+++ ArduinoUno/ArduinoUno.ino
@@ -1,4 +1,5 @@
 #include <AFMotor.h>
+#include <Servo.h> 
 
 AF_DCMotor motorForwardRight(2);
 AF_DCMotor motorForwardLeft(3);
@@ -6,7 +7,10 @@ AF_DCMotor motorBackRight(1);
 AF_DCMotor motorBackLeft(4);
 int speed = 0;
 int steering = 0;
-bool steeringEnabled = 0;
+bool steeringEnabled = 0;  //true when the servo is used
+
+Servo steeringSero;	// create servo object to control a servo
+int servoPos = 0;
 
 //////////////////////SETTINGS
 #define SPEED_PIN A2             //speed analog pin
@@ -18,11 +22,16 @@ void setup() {
   pinMode(SPEED_PIN, INPUT);
   pinMode(STEERING_PIN, INPUT);
   pinMode(STEERING_ENABLE_PIN, INPUT);
+
+  steeringSero.attach(10);  
 }
 
 void loop() {
   retrieveValues();
   handleMotors();
+
+// for(servoPos = 0; servoPos <= 180; servoPos += 1) 
+		// steeringSero.write(0);
 
   // printValues();
   // printValueTimings();
@@ -62,39 +71,45 @@ void handleMotors(){
   motorBackRight.setSpeed(abs(speed));
   motorBackLeft.setSpeed(abs(speed));
 
-  if (abs(speed) < 20 || (steeringEnabled && abs(steering) < 20)) {
+  if(abs(speed) < 20){
     motorForwardRight.run(RELEASE);
     motorForwardLeft.run(RELEASE);
     motorBackRight.run(RELEASE);
     motorBackLeft.run(RELEASE);
   }
-  if (speed >= 20) {
-    motorForwardRight.run(BACKWARD);
-    motorForwardLeft.run(FORWARD);
-    motorBackRight.run(BACKWARD);
-    motorBackLeft.run(FORWARD);
-  } else if (speed <= -20) {
-    motorForwardRight.run(FORWARD);
-    motorForwardLeft.run(BACKWARD);
-    motorBackRight.run(FORWARD);
-    motorBackLeft.run(BACKWARD);
+  else if(speed >= 20){
+      motorForwardRight.run(FORWARD);
+      motorForwardLeft.run(FORWARD);
+      motorBackRight.run(FORWARD);
+      motorBackLeft.run(FORWARD);
+  }else if(speed <= -20){
+      motorForwardRight.run(BACKWARD);
+      motorForwardLeft.run(BACKWARD);
+      motorBackRight.run(BACKWARD);
+      motorBackLeft.run(BACKWARD);
   }
 
-  if (steeringEnabled && abs(steering) >= 20) {
+  if(abs(steering) < 10)
+    servoPos = 90;
+  else
+    servoPos = map(steering, -255, 255, 90+60, 60);
+	steeringSero.write(servoPos);
+
+  if(!steeringEnabled && abs(steering) > 20 && abs(speed) < 255/2){
     motorForwardRight.setSpeed(abs(steering));
     motorForwardLeft.setSpeed(abs(steering));
     motorBackRight.setSpeed(abs(steering));
     motorBackLeft.setSpeed(abs(steering));
 
     if (steering >= 20) {
-      motorForwardRight.run(FORWARD);
+      motorForwardRight.run(BACKWARD);
       motorForwardLeft.run(FORWARD);
-      motorBackRight.run(FORWARD);
+      motorBackRight.run(BACKWARD);
       motorBackLeft.run(FORWARD);
     } else if (steering <= -20) {
-      motorForwardRight.run(BACKWARD);
+      motorForwardRight.run(FORWARD);
       motorForwardLeft.run(BACKWARD);
-      motorBackRight.run(BACKWARD);
+      motorBackRight.run(FORWARD);
       motorBackLeft.run(BACKWARD);
     }
   }
