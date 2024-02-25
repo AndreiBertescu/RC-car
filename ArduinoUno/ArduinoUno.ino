94361ed7afacbdcf4895dd257d6f2d9061d5ceee
Andrei Bertescu <andrei.bertescu2@gmail.com>
2024-02-25 15:07:45 +0200

    Changed ArduinoUno.ino code and updated photos

--- ArduinoUno/ArduinoUno.ino
+++ ArduinoUno/ArduinoUno.ino
@@ -1,5 +1,5 @@
 #include <AFMotor.h>
-#include <Servo.h> 
+#include <Servo.h>
 
 AF_DCMotor motorForwardRight(2);
 AF_DCMotor motorForwardLeft(3);
@@ -9,13 +9,13 @@ int speed = 0;
 int steering = 0;
 bool steeringEnabled = 0;  //true when the servo is used
 
-Servo steeringSero;	// create servo object to control a servo
+Servo steeringSero;  // create servo object to control a servo
 int servoPos = 0;
 
 //////////////////////SETTINGS
-#define SPEED_PIN A2             //speed analog pin
-#define STEERING_PIN A1          //steering analog pin
-#define STEERING_ENABLE_PIN A0   //steering lock analog/digital pin
+#define SPEED_PIN A2            //speed analog pin
+#define STEERING_PIN A1         //steering analog pin
+#define STEERING_ENABLE_PIN A0  //steering lock analog/digital pin
 void setup() {
   Serial.begin(9600);
 
@@ -23,15 +23,12 @@ void setup() {
   pinMode(STEERING_PIN, INPUT);
   pinMode(STEERING_ENABLE_PIN, INPUT);
 
-  steeringSero.attach(10);  
+  steeringSero.attach(10);
 }
 
 void loop() {
   retrieveValues();
   handleMotors();
-
-// for(servoPos = 0; servoPos <= 180; servoPos += 1) 
-		// steeringSero.write(0);
 
   // printValues();
   // printValueTimings();
@@ -57,7 +54,7 @@ void printValueTimings() {
   delay(100);
 }
 
-void retrieveValues(){
+void retrieveValues() {
   speed = map(pulseIn(SPEED_PIN, HIGH, 980000), 20, 975, -255, 255);
   steering = map(pulseIn(STEERING_PIN, HIGH, 980000), 20, 975, -255, 255);
   speed = (abs(speed) < 1 || abs(speed) >= 256) ? 0 : speed / 10 * 10;
@@ -65,48 +62,46 @@ void retrieveValues(){
   steeringEnabled = digitalRead(STEERING_ENABLE_PIN);
 }
 
-void handleMotors(){
+void handleMotors() {
   motorForwardRight.setSpeed(abs(speed));
   motorForwardLeft.setSpeed(abs(speed));
   motorBackRight.setSpeed(abs(speed));
   motorBackLeft.setSpeed(abs(speed));
 
-  if(abs(speed) < 20){
+  if (abs(speed) < 100) {
     motorForwardRight.run(RELEASE);
     motorForwardLeft.run(RELEASE);
     motorBackRight.run(RELEASE);
     motorBackLeft.run(RELEASE);
-  }
-  else if(speed >= 20){
-      motorForwardRight.run(FORWARD);
-      motorForwardLeft.run(FORWARD);
-      motorBackRight.run(FORWARD);
-      motorBackLeft.run(FORWARD);
-  }else if(speed <= -20){
-      motorForwardRight.run(BACKWARD);
-      motorForwardLeft.run(BACKWARD);
-      motorBackRight.run(BACKWARD);
-      motorBackLeft.run(BACKWARD);
+  } else if (speed >= 100) {
+    motorForwardRight.run(FORWARD);
+    motorForwardLeft.run(FORWARD);
+    motorBackRight.run(FORWARD);
+    motorBackLeft.run(FORWARD);
+  } else if (speed <= -100) {
+    motorForwardRight.run(BACKWARD);
+    motorForwardLeft.run(BACKWARD);
+    motorBackRight.run(BACKWARD);
+    motorBackLeft.run(BACKWARD);
   }
 
-  if(abs(steering) < 10)
-    servoPos = 90;
-  else
-    servoPos = map(steering, -255, 255, 90+60, 60);
-	steeringSero.write(servoPos);
-
-  if(!steeringEnabled && abs(steering) > 20 && abs(speed) < 255/2){
+  if (steeringEnabled) {
+    if (abs(steering) > 50)
+      steeringSero.write(map(steering, -255, 255, 135, 45));
+    else
+      steeringSero.write(90);
+  } else if(abs(steering) > 100 && abs(speed) < 100){
     motorForwardRight.setSpeed(abs(steering));
     motorForwardLeft.setSpeed(abs(steering));
     motorBackRight.setSpeed(abs(steering));
     motorBackLeft.setSpeed(abs(steering));
 
-    if (steering >= 20) {
+    if (steering <= -100) {
       motorForwardRight.run(BACKWARD);
       motorForwardLeft.run(FORWARD);
       motorBackRight.run(BACKWARD);
       motorBackLeft.run(FORWARD);
-    } else if (steering <= -20) {
+    } else if (steering >= 100) {
       motorForwardRight.run(FORWARD);
       motorForwardLeft.run(BACKWARD);
       motorBackRight.run(FORWARD);
